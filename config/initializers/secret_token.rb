c2618d6a843bad54b0096ab616d65104ac9595c4
Sanghoon Kwak <sanghoon.kwak@harulabs.com>
2013-09-03 00:40:41 +0900

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Adrupt::Application.config.secret_key_base = '1776ee70ca693bb0e59f991e05f64e7d3c295e963491c2cff6ffa9e660bbaaf209dd7d98422f6fe1889cc367cf012efc16880e791b70f66aef4201ca0e831930'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Adrupt::Application.config.session_store :cookie_store, key: '_adrupt_session'
